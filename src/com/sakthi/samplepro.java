8662491b7dfd935859339e06eac315a5b4d6f96a
SakthiSri15052001 <sakthisrikeerthana@gmail.com>
2022-04-06 10:57:50 +0530

    first

---
+++ src/com/sakthi/samplepro.java
@@ -0,0 +1,13 @@
+package com.sakthi;
+
+public class samplepro {
+   public static void main(String args[])
+   {
+  System.out.println("hi");
+    for(int i=0;i<=10;i++)
+    {
+	  System.out.println(i);
+    }
+   }
+  
+}
